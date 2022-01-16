83d808dcb7d431f0470ef20c7f356c2e800cf221
375297158888 <newatlant@tut.by>
2022-01-16 18:58:36 +0300

    16/01

--- src/OneNutTwoBoys.java
+++ src/OneNutTwoBoys.java
@@ -4,6 +4,13 @@ import java.util.List;
 
 public class OneNutTwoBoys {
     public static void main(String[] args) {
+        TallBoy tallBoy = new TallBoy();
+        Boy boy1 = new LittleBoy(tallBoy);
+        Boy boy2 = new LittleBoy(tallBoy);
+        tallBoy.addBoy(boy1);
+        tallBoy.addBoy(boy2);
+        boy1.request(1);
+        tallBoy.response();
 
     }
 }
@@ -31,16 +38,43 @@ class TallBoy implements Mediator {
     private int total = 0;
 
     public void addBoy(Boy boy) {
-    boys.add(boy);
+        boys.add(boy);
     }
 
     @Override
     public void response() {
+        int countOfBoys = boys.size();
+        int count = total / countOfBoys;
+        int remainder = total % countOfBoys;
+        for (Boy currBoy : boys) {
+            //don't tell ourselves
+            currBoy.receive(count);
+        }
+        System.out.println("Mediator: Total count of nuts is " + total + ". " + remainder + " nuts is (are) mine.");
+    }
+
+
+    @Override
+    public void setTotal(int total) {
+        this.total = total;
+    }
+}
+
+class LittleBoy extends Boy {
+
+    public LittleBoy(Mediator mediator) {
+        super(mediator);
+    }
+
+    @Override
+    public void receive(int count) {
+        System.out.println("Boy: I got " + count + " nuts. ");
 
     }
 
     @Override
-    public void setTotal(int total) {
-    this.total = total;
+    public void request(int count) {
+        mediator.setTotal(count);
+
     }
 }
